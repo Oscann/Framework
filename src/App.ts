a02bc79cd78b51c7a6f6dddf839f3b7dc20cf4c5
Oscann <oscandido10@gmail.com>
2023-06-19 09:17:18 -0300

    implemented general way of exporting pages

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import fs from 'fs'
 import { formatURL, getPagesPaths } from './utils'
-import t from '../src/pages/index'
+import t from './pages/index'
 
 const app = express()
 app.use(express.json())
@@ -12,13 +12,13 @@ const urls = formatURL(files)
 console.log(urls)
 
 for (const url of urls) {
-    import(`../src/pages${url}`).then(console.log)
-
-    app.get(url, (req, res) => {
-        res.send("Hello, World!")
+    import(`./pages${url}`).then(pageData => {
+        app.get(url, (req, res) => {
+            res.send(pageData.default())
+        })
     })
 }
 
 app.listen(3000, () => {
-    console.log("HERE")
+    console.log("Running...")
 })
